a09843293e9c623b98e19b109b49588906d6b155
kaw393939 <keith@webizly.com>
2018-04-23 19:28:09 -0400

    Project Forms

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -29,7 +29,11 @@ class QuestionController extends Controller
      */
     public function create()
     {
-        //
+
+        $question = new Question;
+        $edit = FALSE;
+        return view('questionForm', ['question' => $question,'edit' => $edit  ]);
+
     }
 
     /**
@@ -40,8 +44,28 @@ class QuestionController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $input = $request->validate([
+            'body' => 'required|min:5',
+        ], [
+
+            'body.required' => 'Body is required',
+            'body.min' => 'Body must be at least 5 characters',
+
+        ]);
+        $input = request()->all();
+
+        $question = new Question($input);
+        $question->user()->associate(Auth::user());
+        $question->save();
+
+        return redirect()->route('home')->with('message', 'IT WORKS!');
+
+
+
+        // return redirect()->route('questions.show', ['id' => $question->id]);
+
     }
+
 
     /**
      * Display the specified resource.
@@ -60,9 +84,10 @@ class QuestionController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function edit($id)
+    public function edit(Question $question)
     {
-        //
+        $edit = TRUE;
+        return view('questionForm', ['question' => $question, 'edit' => $edit ]);
     }
 
     /**
@@ -72,9 +97,22 @@ class QuestionController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(Request $request, Question $question)
     {
-        //
+
+        $input = $request->validate([
+            'body' => 'required|min:5',
+        ], [
+
+            'body.required' => 'Body is required',
+            'body.min' => 'Body must be at least 5 characters',
+
+        ]);
+
+        $question->body = $request->body;
+        $question->save();
+
+        return redirect()->route('questions.show',['question_id' => $question->id])->with('message', 'Saved');
     }
 
     /**
@@ -83,8 +121,10 @@ class QuestionController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(Question $question)
     {
-        //
+        $question->delete();
+        return redirect()->route('home')->with('message', 'Deleted');
+
     }
 }
