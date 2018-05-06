6ef3a77c04ffde5d7f5ea8d0853b40246b2d4563
varnelle <vpm3@njit.edu>
2018-05-06 18:37:58 -0400

    Update app.blade.php and faq.blade.php

--- resources/views/faq.blade.php
+++ resources/views/faq.blade.php
@@ -2,10 +2,50 @@
 
 @section('content')
 
+    <header>
+        <nav class="navbar navbar-expand-md navbar-blue fixed-top bg-dark">
+            <a class="navbar-brand" href="#">List of FAQ Views</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse"
+                    aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarCollapse">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
+                    </li>
+
+
+                    class="nav-item">
+                    <a class="nav-link" href="/register">Registration</a>
+                    </li>
+                    class="nav-item">
+                    <a class="nav-link" href="/login">Login</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </header>
+    <div class="container">
+        <div class="row ">
+            <div class="col-md-12">
+
     <h1>List of Available FAQ Views Include:</h1>
 
-    <h2>Profile</h2>
-    <h3>Question</h3>
-    <h4>Answer</h4>
+     <div>
+                    <a href="{{ url('/questions/create') }}">Question</a>
+                    <h1>You can create questions, edit questions and view questions by clicking on the above link.</h1>
+                    <a href="{{ url('/user/profile_id/profile/profile_id')}}">Profile</a>
+                    <h2>You can view the profile of the logged in user on the above page. You must be logged in to view your profile.</h2>
+                    <a href="https://github.com/varnelle/faq_final/projects/1#card-9456982">User Story</a>
+                    <h3>Github User Story can be viewed at the above link.</h3>
+
+
+                </div>
+
+            </div>
+        </div>
+    </div>
+
 
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -37,8 +37,6 @@
             @endguest
 
 
-
-
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
@@ -103,11 +101,6 @@
 
             <!-- Collect the nav links, forms, and other content for toggling -->
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                <ul class="nav navbar-nav">
-                    <li><a href="answer">ANS</a></li>
-                    <li><a href="profile">Profile</a></li>
-                    <li><a href="question">Ques?</a></li>
-                </ul>
 
                 <ul class="nav navbar-nav navbar-left">
                     <li><a href="#">latest question here</a></li>
