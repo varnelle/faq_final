f563ebfd8a8b1fd35e582d034700ffd7aa53098e
varnelle <vpm3@njit.edu>
2018-05-06 16:42:12 -0400

    Create new view for FAQ View listing

---
+++ resources/views/faq.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <h1>List of Available FAQ Views Include:</h1>
+
+    <h2>Profile</h2>
+    <h3>Question</h3>
+    <h4>Answer</h4>
+
+@endsection
